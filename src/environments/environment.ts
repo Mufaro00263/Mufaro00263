23cef60e8456bd7c458e81132cf51042630aabe0
Mufaro00263 <mmm0134@mix.wvu.edu>
2021-11-04 14:54:27 -0400

    Lab 9

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'discord-app-ba936',
+    appId: '1:991538320636:web:a6bf9121de6792a0b3f379',
+    databaseURL: 'https://discord-app-ba936-default-rtdb.firebaseio.com',
+    storageBucket: 'discord-app-ba936.appspot.com',
+    apiKey: 'AIzaSyD-bi4bLMXyOleJpTKr7lyqVSahThW2RtU',
+    authDomain: 'discord-app-ba936.firebaseapp.com',
+    messagingSenderId: '991538320636',
+    measurementId: 'G-JQK1986K2V',
+  },
   production: false
 };
 
